a488ba131a64ec0ca8a866443a75f0f79aee01ae
Lee, Sang Moon <moonysl@hanmail.net>
2020-09-18 12:57:53 +0900

    handle additonal mode and image-changing

--- light-dark-mode/script.js
+++ light-dark-mode/script.js
@@ -6,11 +6,36 @@ const image2 = document.getElementById("image2");
 const image3 = document.getElementById("image3");
 const textBox = document.getElementById("text-box");
 
+function darkMode() {
+  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  toggleIcon.children[0].textContent = "Dark Mode";
+  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+  imageMode("dark")
+}
+
+function lightMode() {
+  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  toggleIcon.children[0].textContent = "Dark Mode";
+  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+  imageMode("light");
+}
+
+function imageMode(mode) {
+  image1.src = `img/undraw_proud_coder_${mode}.svg`;
+  image2.src = `img/undraw_feeling_proud_${mode}.svg`;
+  image3.src = `img/undraw_conceptual_idea_${mode}.svg`;
+}
+
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
+    darkMode();
+
   } else {
     document.documentElement.setAttribute("data-theme", "light");
+    lightMode();
   }
 }
 
