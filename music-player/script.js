ad655b3c81c35988222dc03f4bd6737e72a9ccbf
moonyl <moonysl@hanmail.net>
2020-09-26 10:41:00 +0900

    update progress bar

--- music-player/script.js
+++ music-player/script.js
@@ -2,6 +2,8 @@ const image = document.querySelector("img");
 const title = document.getElementById("title");
 const artist = document.getElementById("artist");
 const music = document.querySelector("audio");
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
@@ -75,5 +77,18 @@ function nextSong() {
   playSong();
 }
 
+loadSong(songs[songIndex]);
+
+function updateProgressBar(e) {
+  if (isPlaying) {
+    //console.log(e);
+    const { duration, currentTime } = e.srcElement;
+    console.log({ duration, currentTime });
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+  }
+}
+
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
+music.addEventListener("timeupdate", updateProgressBar);
